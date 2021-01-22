77b4596a367740c2142588d3c0dc10417b32c298
Justin Snapp <justin.snapp@taraxa.io>
2021-01-22 12:43:18 -0800

    fix for crashing when improper address

--- worker/faucet.js
+++ worker/faucet.js
@@ -13,37 +13,56 @@ const account = taraxa.accounts.privateKeyToAccount(config.faucet.privateKey);
 
 let unconfirmed = 0;
 
+function getRandomSubarray(arr, size) {
+    var shuffled = arr.slice(0), i = arr.length, min = i - size, temp, index;
+    while (i-- > min) {
+        index = Math.floor((i + 1) * Math.random());
+        temp = shuffled[index];
+        shuffled[index] = shuffled[i];
+        shuffled[i] = temp;
+    }
+    return shuffled.slice(min);
+}
+
 async function drip() {
     if (unconfirmed < config.faucet.maxUnconfirmed) {
         const cups = await Faucet.find().sort({created: -1}).limit(config.faucet.maxUnconfirmed - unconfirmed);
         for(let cup of cups) {
             const fn = await FaucetNonce.findOneAndUpdate({}, { $inc: { nonce: 1 }}, {upsert: true, new: true});
-            const tx = {
-                from: account.address,
-                to: cup.address,
-                value: 1 * 1e17,
-                gas: 21000,
-                gasPrice: 1 * 1e9,
-                nonce: fn.nonce - 1
-            };
-            const signed = await account.signTransaction(tx);
-            await new Promise((resolve, reject) => {
-                let transactionHash = "";
-                taraxa.sendSignedTransaction(signed.rawTransaction)
-                    .once('transactionHash', txHash => {
-                        console.log('Sending tx', txHash, 'to', cup.address, 'from', account.address, 1 * 1e10);
-                        transactionHash = txHash;
-                        unconfirmed++;
-                        resolve(txHash);
-                    })
-                    .once('receipt', function(receipt){ 
-                        unconfirmed--;
-                    })
-                    .once('error', error => {
-                        console.error(error);
-                        reject(error);
-                    })
-            });
+            
+            try {
+
+                const tx = {
+                    from: account.address,
+                    to: cup.address,
+                    value: 1 * 1e17,
+                    gas: 21000,
+                    gasPrice: 1 * 1e9,
+                    nonce: fn.nonce - 1
+                };
+                const signed = await account.signTransaction(tx);
+
+                await new Promise((resolve, reject) => {
+                    let transactionHash = "";
+                    taraxa.sendSignedTransaction(signed.rawTransaction)
+                        .once('transactionHash', txHash => {
+                            console.log('Sending tx', txHash, 'to', cup.address, 'from', account.address, 1 * 1e10);
+                            transactionHash = txHash;
+                            unconfirmed++;
+                            resolve(txHash);
+                        })
+                        .once('receipt', function(receipt){ 
+                            unconfirmed--;
+                        })
+                        .once('error', error => {
+                            console.error(error);
+                            reject(error);
+                        })
+                });
+
+            } catch (e) {
+                console.error(e);
+            }
         }
     } else {
         console.log('Skipping a round due to unconfirmed count', unconfirmed);
