c36b89be35ad0cb24af4fb1e49c2bfb854c234d7
Leonard Mocanu <leonard.mocanu@ymail.com>
2021-04-26 15:26:14 +0300

    fix(faucet): adds address validation to the faucet API

--- pages/api/faucet/[id].js
+++ pages/api/faucet/[id].js
@@ -1,28 +1,46 @@
-import config from 'config';
-import mongoose from 'mongoose';
+import config from "config";
+import mongoose from "mongoose";
+import Web3Utils from "web3-utils";
 
-import Faucet from '../../../models/faucet';
+import Faucet from "../../../models/faucet";
 
 export default async function userHandler(req, res) {
-    try {
-        mongoose.connection._readyState || await mongoose.connect(config.mongo.uri, config.mongo.options);
-    } catch (e) {
-        console.error(e);
-        return res.status(500).json({error: 'Internal error. Please try your request again.'});
-    }
-    const {
-        query: {id},
-        method,
-    } = req;
+  try {
+    mongoose.connection._readyState ||
+      (await mongoose.connect(config.mongo.uri, config.mongo.options));
+  } catch (e) {
+    console.error(e);
+    return res
+      .status(500)
+      .json({ error: "Internal error. Please try your request again." });
+  }
+  const {
+    query: { id },
+  } = req;
 
-    try {
-        await Faucet.findOneAndUpdate({address: id}, {address: id}, {upsert: true, new: true});
-        res.json({
-            status: `Address ${id} is in faucet queue.`
-        })
-    } catch (e) {
-        console.error(e);
-        res.status(500).json({error: 'Internal error. Please try your request again.'});
-    }
-            
+  let address = id;
+  if (typeof id !== "string") {
+    address = address.toString();
+  }
+  address = address.trim();
+
+  if (!Web3Utils.isAddress(address)) {
+    res.status(400).json({ error: "Wallet Address is not valid" });
+  }
+
+  try {
+    await Faucet.findOneAndUpdate(
+      { address },
+      { address },
+      { upsert: true, new: true }
+    );
+    res.json({
+      status: `Address ${address} is in faucet queue.`,
+    });
+  } catch (e) {
+    console.error(e);
+    res
+      .status(500)
+      .json({ error: "Internal error. Please try your request again." });
+  }
 }
