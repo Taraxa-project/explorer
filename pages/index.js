ce10afa56db78124db3d081cb6e76d5656181841
Leonard Mocanu <leonard.mocanu@ymail.com>
2021-06-03 18:43:08 +0300

    fix(dashboard): non negative numbers in the tps, block time and DAGps

--- pages/index.js
+++ pages/index.js
@@ -52,8 +52,8 @@ function Index({recentBlocks, recentDagBlocks}) {
     reversedRecentBlocks.forEach((block, index) => {
       if (index !== 0) {
         let previousBlockTimestamp = reversedRecentBlocks[index - 1].timestamp;
-        const elapsedSeconds = Math.round(new Date(block.timestamp).valueOf() / 1000) - Math.round(new Date(previousBlockTimestamp).valueOf() / 1000)
-        const tps = block.transactions.length / elapsedSeconds;
+        const elapsedSeconds = Math.abs(Math.round(new Date(block.timestamp).valueOf() / 1000) - Math.round(new Date(previousBlockTimestamp).valueOf() / 1000))
+        const tps = Number(block.transactions.length / elapsedSeconds).toFixed(1);
         newTpsData.push({
           block: block.number,
           tps,
@@ -83,7 +83,7 @@ function Index({recentBlocks, recentDagBlocks}) {
           const dagBlocks = dpsData[period];
           const dagStart = new Date(dagBlocks[0].timestamp).valueOf();
           const dagEnd = new Date(dagBlocks[dagBlocks.length - 1].timestamp).valueOf();
-          const durationSeconds = Math.round(dagEnd / 1000) - Math.round(dagStart / 1000)
+          const durationSeconds = Math.abs(Math.round(dagEnd / 1000) - Math.round(dagStart / 1000))
           if (durationSeconds) {
             newDps.push({
               period: period.toString(),
