da6a93289efee934a4ef38c3e3c5db2d7784bf61
Leonard Mocanu <leonard.mocanu@ymail.com>
2021-06-13 23:28:24 +0300

    fix(delegation) adds new delegation model

--- models/delegate.js
+++ models/delegate.js
@@ -3,11 +3,22 @@ const mongoose = require("mongoose");
 const Delegate = new mongoose.Schema(
   {
     _id: { type: String },
-    value: { type: String, default: "0" },
-    created: { type: Date, default: Date.now, index: true },
+    counterpart: {
+      type: String,
+      default: "0x0000000000000000000000000000000000000000",
+    },
+    valueToAdd: { type: String, default: "0" },
+    valueToSubstract: { type: String, default: "0" },
+    total: { type: String, default: "0" },
+    status: {
+      type: String,
+      enum: ["QUEUED", "FINISHED"],
+      default: "QUEUED",
+    },
   },
   {
     versionKey: false,
+    timestamps: true,
   }
 );
 
