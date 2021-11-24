b6dbaa147957e4888d5b0398f5baa694048cbc66
Leonard Mocanu <leonard.mocanu@ymail.com>
2021-11-24 17:56:32 +0200

    adds mining stats api

---
+++ pages/api/address/[id]/stats.js
@@ -0,0 +1,117 @@
+import moment from 'moment';
+import withApiHandler from '../../../../lib/api-handler';
+
+import Block from '../../../../models/block';
+
+async function handler(req, res) {
+  const {
+    method,
+    query: { id },
+  } = req;
+
+  switch (method) {
+    case 'GET':
+      try {
+        let totalProduced = 0;
+        let lastBlockTimestamp = null;
+        let rank = null;
+        let produced = 0;
+
+        const blocksProduced = await Block.aggregate([
+          { $match: { author: id } },
+          {
+            $group: {
+              _id: '$author',
+              count: { $sum: 1 },
+              lastBlockTimestamp: { $last: '$timestamp' },
+            },
+          },
+        ]);
+
+        if (blocksProduced.length) {
+          totalProduced = blocksProduced[0].count;
+          lastBlockTimestamp = blocksProduced[0].lastBlockTimestamp;
+
+          const week = moment().isoWeek();
+          const year = moment().isoWeekYear();
+
+          const now = moment().isoWeekYear(year).isoWeek(week);
+          const firstDay = now.startOf('week').toDate();
+          const lastDay = now.endOf('week').toDate();
+
+          const ranking = await Block.aggregate([
+            {
+              $match: {
+                timestamp: {
+                  $gte: firstDay,
+                  $lte: lastDay,
+                },
+              },
+            },
+            {
+              $group: {
+                _id: '$author',
+                lastBlockNumber: { $last: '$number' },
+                count: { $sum: 1 },
+              },
+            },
+            {
+              $sort: {
+                count: -1,
+                lastBlockNumber: 1,
+              },
+            },
+            {
+              $group: {
+                _id: {},
+                rank: {
+                  $push: {
+                    _id: '$_id',
+                    count: '$count',
+                  },
+                },
+              },
+            },
+            {
+              $unwind: {
+                path: '$rank',
+                includeArrayIndex: 'index',
+              },
+            },
+            {
+              $project: {
+                _id: '$rank._id',
+                count: '$rank.count',
+                index: '$index',
+              },
+            },
+            {
+              $match: {
+                _id: id,
+              },
+            },
+          ]);
+
+          if (ranking.length) {
+            rank = ranking[0].index + 1;
+            produced = ranking[0].count;
+          }
+        }
+        return res.json({
+          totalProduced,
+          lastBlockTimestamp,
+          rank,
+          produced,
+        });
+      } catch (e) {
+        console.error(e);
+        res.status(500).json({ error: 'Internal error. Please try your request again.' });
+      }
+      break;
+    default:
+      res.setHeader('Allow', ['GET']);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
+}
+
+export default withApiHandler(handler);
