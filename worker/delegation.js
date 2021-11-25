a51ec02570df74a8adad941ecf8f49cb19967bb7
Carlos Lage <carlos.lage@taraxa.io>
2021-11-25 20:05:47 +0000

    Add appsignal support on backend

--- worker/delegation.js
+++ worker/delegation.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 
 const config = require('config');
+const { Appsignal } = require('@appsignal/nodejs');
+
+const appsignal = new Appsignal(config.appsignal);
+const tracer = appsignal.tracer();
+const rootSpan = tracer.createSpan({ namespace: 'background' }).setName('delegation-worker');
+
 const BN = require('bn.js');
 const Web3 = require('web3');
 const RLP = require('rlp');
@@ -13,110 +19,135 @@ const sleep = async (delay = 30 * 1000) => {
 };
 
 async function worker() {
-  const { Delegate } = await useDb();
+  tracer.withSpan(tracer.createSpan().setName('worker'), async (span) => {
+    console.log('worker span');
+    console.log(span);
 
-  const delegates = await Delegate.find({
-    status: 'QUEUED',
-  }).sort({
-    updatedAt: 1,
+    const { Delegate } = await useDb();
+
+    const delegates = await Delegate.find({
+      status: 'QUEUED',
+    }).sort({
+      updatedAt: 1,
+    });
+    if (delegates.length === 0) {
+      console.log('No delegation requests!');
+      span.close(); //don't measure sleep for performance
+      return await sleep();
+    }
+
+    for (let delegate of delegates) {
+      tracer.withSpan(tracer.createSpan().setName('delegate'), async (span) => {
+        console.log('Processing delegation', delegate);
+
+        const valueToAdd = new BN(delegate.valueToAdd);
+        const valueToSubstract = new BN(delegate.valueToSubstract);
+        let total = new BN(delegate.total);
+        let status = 'FINISHED';
+
+        if (valueToAdd.gtn(0)) {
+          console.log('Delegating to', delegate._id, 'value', valueToAdd.toString());
+
+          try {
+            await delegateTransaction(delegate.counterpart, valueToAdd.mul(new BN(2)));
+            console.log('- own node', delegate.counterpart);
+            await delegateTransaction(delegate._id, valueToAdd);
+            console.log('- user node', delegate._id);
+          } catch (e) {
+            tracer.setError(e);
+            console.error(e);
+            status = 'ERROR';
+          }
+
+          total = total.add(valueToAdd);
+        }
+
+        if (valueToSubstract.gtn(0)) {
+          console.log('Undelegating from', delegate._id, 'value', valueToSubstract.toString());
+
+          try {
+            await undelegateTransaction(delegate.counterpart, valueToSubstract.mul(new BN(2)));
+            console.log('- own node', delegate.counterpart);
+            await undelegateTransaction(delegate._id, valueToSubstract);
+            console.log('- user node', delegate._id);
+          } catch (e) {
+            tracer.setError(e);
+            console.error(e);
+            status = 'ERROR';
+          }
+
+          total = total.sub(valueToSubstract);
+        }
+
+        await Delegate.findOneAndUpdate(
+          { _id: delegate._id },
+          {
+            valueToAdd: '0',
+            valueToSubstract: '0',
+            total: total.toString(),
+            status: status,
+          },
+        );
+
+        span.close();
+      });
+
+      span.close(); //don't measure sleep for performance
+      await sleep();
+    }
   });
-  if (delegates.length === 0) {
-    console.log('No delegation requests!');
-    return await sleep();
-  }
-
-  for (let delegate of delegates) {
-    console.log('Processing delegation', delegate);
-
-    const valueToAdd = new BN(delegate.valueToAdd);
-    const valueToSubstract = new BN(delegate.valueToSubstract);
-    let total = new BN(delegate.total);
-    let status = 'FINISHED';
-
-    if (valueToAdd.gtn(0)) {
-      console.log('Delegating to', delegate._id, 'value', valueToAdd.toString());
-
-      try {
-        await delegateTransaction(delegate.counterpart, valueToAdd.mul(new BN(2)));
-        console.log('- own node', delegate.counterpart);
-        await delegateTransaction(delegate._id, valueToAdd);
-        console.log('- user node', delegate._id);
-      } catch (e) {
-        console.error(e);
-        status = 'ERROR';
-      }
-
-      total = total.add(valueToAdd);
-    }
-
-    if (valueToSubstract.gtn(0)) {
-      console.log('Undelegating from', delegate._id, 'value', valueToSubstract.toString());
-
-      try {
-        await undelegateTransaction(delegate.counterpart, valueToSubstract.mul(new BN(2)));
-        console.log('- own node', delegate.counterpart);
-        await undelegateTransaction(delegate._id, valueToSubstract);
-        console.log('- user node', delegate._id);
-      } catch (e) {
-        console.error(e);
-        status = 'ERROR';
-      }
-
-      total = total.sub(valueToSubstract);
-    }
-
-    await Delegate.findOneAndUpdate(
-      { _id: delegate._id },
-      {
-        valueToAdd: '0',
-        valueToSubstract: '0',
-        total: total.toString(),
-        status: status,
-      },
-    );
-
-    await sleep();
-  }
 }
 
 async function delegateTransaction(address, value) {
-  await sendTransaction(`0x${bufferToHex(RLP.encode([[address, [value, 0]]]))}`);
+  tracer.withSpan(tracer.createSpan().setName('undelegateTransaction'), async (span) => {
+    console.log('delegateTransaction span');
+    console.log(span);
+    await sendTransaction(`0x${bufferToHex(RLP.encode([[address, [value, 0]]]))}`);
+  });
 }
 
 async function undelegateTransaction(address, value) {
-  await sendTransaction(`0x${bufferToHex(RLP.encode([[address, [value, 1]]]))}`);
+  tracer.withSpan(tracer.createSpan().setName('undelegateTransaction'), async (span) => {
+    console.log('undelegateTransaction span');
+    console.log(span);
+    await sendTransaction(`0x${bufferToHex(RLP.encode([[address, [value, 1]]]))}`);
+  });
 }
 
 async function sendTransaction(input) {
-  const { FaucetNonce } = await useDb();
-  const taraxa = new Web3(config.taraxa.node.http);
-  const account = taraxa.eth.accounts.privateKeyToAccount(config.faucet.privateKey);
-  const faucetNonce = await FaucetNonce.findOneAndUpdate(
-    {},
-    { $inc: { nonce: 1 } },
-    { upsert: true, new: true },
-  );
-  const tx = await account.signTransaction({
-    from: account.address,
-    to: '0x00000000000000000000000000000000000000ff',
-    value: 0,
-    gas: 30000,
-    gasPrice: 0,
-    nonce: faucetNonce.nonce - 1,
-    input,
-  });
+  tracer.withSpan(tracer.createSpan().setName('sendTransaction'), async (span) => {
+    console.log('sendTransaction span');
+    console.log(span);
+    const { FaucetNonce } = await useDb();
+    const taraxa = new Web3(config.taraxa.node.http);
+    const account = taraxa.eth.accounts.privateKeyToAccount(config.faucet.privateKey);
+    const faucetNonce = await FaucetNonce.findOneAndUpdate(
+      {},
+      { $inc: { nonce: 1 } },
+      { upsert: true, new: true },
+    );
+    const tx = await account.signTransaction({
+      from: account.address,
+      to: '0x00000000000000000000000000000000000000ff',
+      value: 0,
+      gas: 30000,
+      gasPrice: 0,
+      nonce: faucetNonce.nonce - 1,
+      input,
+    });
 
-  await new Promise((resolve, reject) => {
-    taraxa.eth
-      .sendSignedTransaction(tx.rawTransaction)
-      .on('transactionHash', (txHash) => {
-        waitForTransaction(taraxa, txHash).then(() => {
-          resolve(txHash);
+    await new Promise((resolve, reject) => {
+      taraxa.eth
+        .sendSignedTransaction(tx.rawTransaction)
+        .on('transactionHash', (txHash) => {
+          waitForTransaction(taraxa, txHash).then(() => {
+            resolve(txHash);
+          });
+        })
+        .on('error', (error) => {
+          reject(error);
         });
-      })
-      .on('error', (error) => {
-        reject(error);
-      });
+    });
   });
 }
 
@@ -128,50 +159,64 @@ function waitForTransaction(web3, txnHash, options = null) {
   const interval = options && options.interval ? options.interval : 500;
   const blocksToWait = options && options.blocksToWait ? options.blocksToWait : 1;
   const transactionReceiptAsync = async function (txnHash, resolve, reject) {
-    try {
-      var receipt = web3.eth.getTransactionReceipt(txnHash);
-      if (!receipt) {
-        setTimeout(function () {
-          transactionReceiptAsync(txnHash, resolve, reject);
-        }, interval);
-      } else {
-        if (blocksToWait > 0) {
-          var resolvedReceipt = await receipt;
-          if (!resolvedReceipt || !resolvedReceipt.blockNumber) {
-            setTimeout(function () {
-              transactionReceiptAsync(txnHash, resolve, reject);
-            }, interval);
-          } else {
-            try {
-              var block = await web3.eth.getBlock(resolvedReceipt.blockNumber);
-              var current = await web3.eth.getBlock('latest');
-              if (current.number - block.number >= blocksToWait) {
-                var txn = await web3.eth.getTransaction(txnHash);
-                if (txn.blockNumber != null) {
-                  resolve(resolvedReceipt);
+    tracer.withSpan(tracer.createSpan().setName('transactionReceiptAsync'), async (span) => {
+      console.log('transactionReceiptAsync span');
+      console.log(span);
+
+      try {
+        var receipt = web3.eth.getTransactionReceipt(txnHash);
+        if (!receipt) {
+          span.close();
+          setTimeout(function () {
+            transactionReceiptAsync(txnHash, resolve, reject);
+          }, interval);
+        } else {
+          if (blocksToWait > 0) {
+            var resolvedReceipt = await receipt;
+            if (!resolvedReceipt || !resolvedReceipt.blockNumber) {
+              span.close();
+              setTimeout(function () {
+                transactionReceiptAsync(txnHash, resolve, reject);
+              }, interval);
+            } else {
+              try {
+                var block = await web3.eth.getBlock(resolvedReceipt.blockNumber);
+                var current = await web3.eth.getBlock('latest');
+                if (current.number - block.number >= blocksToWait) {
+                  var txn = await web3.eth.getTransaction(txnHash);
+                  if (txn.blockNumber != null) {
+                    span.close();
+                    resolve(resolvedReceipt);
+                  } else {
+                    span.close();
+                    reject(
+                      new Error(`Transaction with hash: ${txnHash} ended up in an uncle block.`),
+                    );
+                  }
                 } else {
-                  reject(
-                    new Error(`Transaction with hash: ${txnHash} ended up in an uncle block.`),
-                  );
+                  span.close();
+                  setTimeout(function () {
+                    transactionReceiptAsync(txnHash, resolve, reject);
+                  }, interval);
                 }
-              } else {
+              } catch (e) {
+                tracer.setError(e);
+                span.close();
                 setTimeout(function () {
                   transactionReceiptAsync(txnHash, resolve, reject);
                 }, interval);
               }
-            } catch (e) {
-              setTimeout(function () {
-                transactionReceiptAsync(txnHash, resolve, reject);
-              }, interval);
             }
+          } else {
+            span.close();
+            resolve(receipt);
           }
-        } else {
-          resolve(receipt);
         }
+      } catch (e) {
+        tracer.setError(e);
+        reject(e);
       }
-    } catch (e) {
-      reject(e);
-    }
+    });
   };
 
   return new Promise(function (resolve, reject) {
@@ -179,14 +224,21 @@ function waitForTransaction(web3, txnHash, options = null) {
   });
 }
 
-(async () => {
+const main = async () => {
+  console.log('Delegation worker started');
+  while (true) {
+    await worker();
+  }
+};
+
+tracer.withSpan(rootSpan, async (span) => {
   try {
-    console.log('Delegation worker started');
-    while (true) {
-      await worker();
-    }
+    await main();
   } catch (e) {
     console.error(e);
+    tracer.setError(e);
+    span.close();
+    appsignal.stop();
     process.exit(1);
   }
-})();
+});
